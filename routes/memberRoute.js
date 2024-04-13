063ac463ca528641d57a5b752012335354098640
git-dariel <104293978+git-dariel@users.noreply.github.com>
2024-04-13 15:03:22 +0800

    update routes

--- routes/memberRoute.js
+++ routes/memberRoute.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {
-  deactivateMember,
-  activateMember,
-  getProjectMembers,
-} = require('../services/memberService');
+const memberService = require('../services/memberService');
 const { API_ENDPOINTS } = require('../config/endpointsConfig');
 
-router.route(API_ENDPOINTS.MEMBER.GET_BY_ID).get(getProjectMembers);
-router.route(API_ENDPOINTS.MEMBER.DEACTIVATE).put(deactivateMember);
-router.route(API_ENDPOINTS.MEMBER.ACTIVATE).put(activateMember);
+router.route(API_ENDPOINTS.MEMBER.GET_BY_ID).get(memberService.getProjectMembers);
+router.route(API_ENDPOINTS.MEMBER.DEACTIVATE).put(memberService.deactivateMember);
+router.route(API_ENDPOINTS.MEMBER.ACTIVATE).put(memberService.activateMember);
 
 module.exports = router;
--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {
-  createProject,
-  getProjects,
-  updateProject,
-  deleteProject,
-  addMemberToProject,
-} = require('../services/projectService');
+const projectService = require('../services/projectService');
 const { API_ENDPOINTS } = require('../config/endpointsConfig');
 
-router.route(API_ENDPOINTS.PROJECT.GET).get(getProjects);
-router.route(API_ENDPOINTS.PROJECT.POST).post(createProject);
-router.route(API_ENDPOINTS.PROJECT.PUT).put(updateProject);
-router.route(API_ENDPOINTS.PROJECT.DELETE).delete(deleteProject);
-router.route(API_ENDPOINTS.MEMBER.POST).post(addMemberToProject);
+router.route(API_ENDPOINTS.PROJECT.GET).get(projectService.getProjects);
+router.route(API_ENDPOINTS.PROJECT.POST).post(projectService.createProject);
+router.route(API_ENDPOINTS.PROJECT.PUT).put(projectService.updateProject);
+router.route(API_ENDPOINTS.PROJECT.DELETE).delete(projectService.deleteProject);
+router.route(API_ENDPOINTS.MEMBER.POST).post(projectService.addMemberToProject);
 
 module.exports = router;
